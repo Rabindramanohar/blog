a2a31a68076e5a9760fe789b6c01ea12d4a16b68
Rabindra Mahato <robinmanohar@gmail.com>
2020-05-27 20:03:16 +0530

    firebase not working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDeErx_pnWncaMHag5-JVT8LGriZJTD2X0",
+    authDomain: "blogsite-756e1.firebaseapp.com",
+    databaseURL: "https://blogsite-756e1.firebaseio.com",
+    projectId: "blogsite-756e1",
+    storageBucket: "blogsite-756e1.appspot.com",
+    messagingSenderId: "302289690009",
+    appId: "1:302289690009:web:6717ba2ad86c4dcb68c708"
+  }
 };
